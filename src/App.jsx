797af28855ea2c1cb756f37bc85a3780d0411b10
MariaSalomea <ramoneustoquia@gmail.com>
2026-08-18 11:40:40 -0600

    solo 2 gifs

--- src/App.jsx
+++ src/App.jsx
@@ -2,46 +2,7 @@ import { useState } from "react";
 import "./App.css";
 
 const gifList = [
-  "altar_todos_santos.gif",
-  "bruja.gif",
-  "calabaza.gif",
-  "central_de_abastos.gif",
-  "corazon.gif",
-  "dia_de_muertos.gif",
-  "diablin.gif",
-  "feliz_navidad.gif",
-  "feliz_navidad_2.gif",
-  "feria.gif",
-  "feria_1.gif",
-  "feria_1_ultrahd.gif",
-  "feria_2.gif",
-  "feria_2_ultrahd.gif",
-  "halloween.gif",
-  "halloween_02.gif",
-  "halloween_03.gif",
-  "halloween_04.gif",
-  "halloween_full.gif",
-  "imss.gif",
-  "imss_2.gif",
-  "navidad.gif",
-  "negro64x16.gif",
-  "negro80x20.gif",
-  "negro128x32.gif",
   "pacman_2.gif",
-  "por_que_me_gustas_wey.gif",
-  "por_que_me_gustas_wey_64_x_32.gif",
-  "predator_01.gif",
-  "predator_02.gif",
-  "publi8020.gif",
-  "san_juditas.gif",
-  "sonic.gif",
-  "spider_joyboy.gif",
-  "viva_mexico.gif",
-  "viva_mexico_2.gif",
-  "viva_mexico_3.gif",
-  "viva_mexico_4.gif",
-  "viva_mexico_5.gif",
-  "viva_mexico_6.gif",
   "viva_mexico_5_ultra.gif",
 ];
 
@@ -56,7 +17,7 @@ const GifGallery = () => {
   const [copiadoIndex, setCopiadoIndex] = useState(null);
 
   // ✅ BASE_URL absoluto correcto para tu sitio GitHub Pages
-  const BASE_URL = "https://ingeabraham23.github.io/gifs/gifs/";
+  const BASE_URL = " https://mariasalomea.github.io/iledprogifs/gifs/";
 
   const copiarLink = async (gifUrl, index) => {
     try {
